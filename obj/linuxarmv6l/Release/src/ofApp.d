6380b3254d96bf1bb38ffddf37335b2a0eefef86
Tarim <git@mediaplaygrounds.co.uk>
2015-10-31 15:34:03 +0000

    David's uncommitted changes

--- obj/linuxarmv6l/Release/src/ofApp.d
+++ obj/linuxarmv6l/Release/src/ofApp.d
@@ -382,7 +382,19 @@ obj/linuxarmv6l/Release/src/ofApp.o: src/ofApp.cpp src/ofApp.h \
  ../../../libs/openFrameworks/video/ofGstVideoPlayer.h \
  ../../../libs/openFrameworks/3d/ofCamera.h \
  ../../../libs/openFrameworks/3d/ofEasyCam.h \
- ../../../addons/ofxDmx/src/ofxDmx.h
+ ../../../addons/ofxDmx/src/ofxDmx.h \
+ ../../../addons/ofxJSON/src/ofxJSON.h \
+ ../../../addons/ofxJSON/libs/jsoncpp/include/json/json.h \
+ ../../../addons/ofxJSON/src/ofxJSONElement.h src/MyTimer.h src/tree.h \
+ ../../../addons/ofxGui/src/ofxGui.h \
+ ../../../addons/ofxGui/src/ofxToggle.h \
+ ../../../addons/ofxGui/src/ofxBaseGui.h \
+ ../../../addons/ofxGui/src/ofxSlider.h \
+ ../../../addons/ofxGui/src/ofxSliderGroup.h \
+ ../../../addons/ofxGui/src/ofxGuiGroup.h \
+ ../../../addons/ofxGui/src/ofxButton.h \
+ ../../../addons/ofxGui/src/ofxPanel.h \
+ ../../../addons/ofxGui/src/ofxLabel.h
 
 src/ofApp.h:
 
@@ -1195,3 +1207,31 @@ src/ofApp.h:
 ../../../libs/openFrameworks/3d/ofEasyCam.h:
 
 ../../../addons/ofxDmx/src/ofxDmx.h:
+
+../../../addons/ofxJSON/src/ofxJSON.h:
+
+../../../addons/ofxJSON/libs/jsoncpp/include/json/json.h:
+
+../../../addons/ofxJSON/src/ofxJSONElement.h:
+
+src/MyTimer.h:
+
+src/tree.h:
+
+../../../addons/ofxGui/src/ofxGui.h:
+
+../../../addons/ofxGui/src/ofxToggle.h:
+
+../../../addons/ofxGui/src/ofxBaseGui.h:
+
+../../../addons/ofxGui/src/ofxSlider.h:
+
+../../../addons/ofxGui/src/ofxSliderGroup.h:
+
+../../../addons/ofxGui/src/ofxGuiGroup.h:
+
+../../../addons/ofxGui/src/ofxButton.h:
+
+../../../addons/ofxGui/src/ofxPanel.h:
+
+../../../addons/ofxGui/src/ofxLabel.h:
